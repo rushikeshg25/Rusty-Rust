0bd4165d00b83a691d192ba41631e694b6a03a5c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-25 16:46:31 +0530

    Created app using cargo

---
+++ cargo-hw/target/debug/cargo-hw.d
@@ -0,0 +1,1 @@
+/home/rushikesh/Projects/Rusty-Rust/cargo-hw/target/debug/cargo-hw: /home/rushikesh/Projects/Rusty-Rust/cargo-hw/src/main.rs
---
+++ cargo-hw/target/debug/deps/cargo_hw-c9275d2a57a4e4e5.d
@@ -0,0 +1,5 @@
+/home/rushikesh/Projects/Rusty-Rust/cargo-hw/target/debug/deps/libcargo_hw-c9275d2a57a4e4e5.rmeta: src/main.rs
+
+/home/rushikesh/Projects/Rusty-Rust/cargo-hw/target/debug/deps/cargo_hw-c9275d2a57a4e4e5.d: src/main.rs
+
+src/main.rs:
