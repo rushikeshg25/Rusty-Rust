31ddc73063662a97e6980569cc1ebb9fe33713bb
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-25 17:04:44 +0530

    Vars,immutable vars and Types

---
+++ basics/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let a=1;   //Immutable
+    let mut b=2;   //Mutable
+    let c:i32=3;   //Immutable
+    let d:bool=true;   //Immutable
+    let mut b=2;
+    let c:i32=3;
+    let d:bool=true;
+
+}
---
+++ cargo-hw/target/debug/deps/cargo_hw-25312ab0a1cde065.d
@@ -0,0 +1,5 @@
+/home/rushikesh/Projects/Rusty-Rust/cargo-hw/target/debug/deps/libcargo_hw-25312ab0a1cde065.rmeta: src/main.rs
+
+/home/rushikesh/Projects/Rusty-Rust/cargo-hw/target/debug/deps/cargo_hw-25312ab0a1cde065.d: src/main.rs
+
+src/main.rs:
