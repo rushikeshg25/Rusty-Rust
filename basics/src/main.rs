8c3e638b4080f72e70973185f588da3b71d00dd1
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-25 17:18:46 +0530

    for loops with vecs,enums

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,50 +1,74 @@
 fn main() {
-    let a=1;   //Immutable
-    let mut b=2;   //Mutable
-    let c:i32=3;   //Immutable
-    let d:bool=true;   //Immutable
-    let mut b=2;
-    let c:i32=3;
-    let d:bool=true;
+    // let a=1;   //Immutable
+    // let mut b=2;   //Mutable
+    // let c:i32=3;   //Immutable
+    // let d:bool=true;   //Immutable
+    // let mut b=2;
+    // let c:i32=3;
+    // let d:bool=true;
 
-    //If else Syntax 
+    // //If else Syntax 
 
-    if true{
-        println!("Hello World from if block");
-    }  
-    else{
-        println!("Hello World from else block");
+    // if true{
+    //     println!("Hello World from if block");
+    // }  
+    // else{
+    //     println!("Hello World from else block");
+    // }
+
+    // //Loop Syntax using Loop keyword
+
+    // let mut i=0;
+    // loop{
+    //     i+=1;
+    //     if i==6{
+    //         continue;
+    //     }
+    //     println!("{}",i);
+    //     if i==10{
+    //         break;
+    //     }
+    // }
+
+    // // Loop Syntax using While keyword
+
+    // i=0;
+    // while i<6{
+    //     i+=1;
+    //     println!("{}",i);
+    //     if i==10{
+    //         break;
+    //     }
+    // }
+
+    // //Loop Syntax using For loop
+
+    // for i in 0..6{
+    //     println!("{}",i);
+    // }
+
+    //For loop using vector
+
+    let animals= vec!["cat","dog","bird"];
+
+    //use this to aviod not be vector accessible instead of below syntax
+    for animal in animals.iter(){
+        println!("{}",animal);
     }
 
-    //Loop Syntax using Loop keyword
-
-    let mut i=0;
-    loop{
-        i+=1;
-        if i==6{
-            continue;
-        }
-        println!("{}",i);
-        if i==10{
-            break;
-        }
+    for (index,animal) in animals.iter().enumerate(){
+        println!("The Index is {} and the animal is {}",index,animal);
     }
 
-    // Loop Syntax using While keyword
-
-    i=0;
-    while i<6{
-        i+=1;
-        println!("{}",i);
-        if i==10{
-            break;
-        }
+    for animal in animals{
+        println!("{}",animal);
     }
 
-    //Loop Syntax using For loop
+    // For loop using array
 
-    for i in 0..6{
-        println!("{}",i);
+    let numbers= [1,2,3,4,5];
+    for number in numbers{
+        println!("{}",number);
     }
 
 }
